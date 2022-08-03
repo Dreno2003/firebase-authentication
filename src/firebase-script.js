1f95543f62f12806bff4a96a22265e32fb5a32fe
Idorenyin Akaininyene <=>
2022-08-03 23:58:14 +0100

    l

--- src/firebase-script.js
+++ src/firebase-script.js
@@ -22,7 +22,7 @@ import {
     updateDoc,
     onSnapshot,
     Timestamp
-} from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js';
+} from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-firestore.js';
 
 // import to update ui
 import {
@@ -36,7 +36,7 @@ import {
 
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-analytics.js";
+//   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
